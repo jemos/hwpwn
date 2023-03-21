6424fdd3003db673cff19865d15e4e3cc6dfdc14
Jean Mousinho <jean.mousinho@gmail.com>
2023-03-21 10:17:59 +0000

    Cleanup of unused libraries, added plot command.

--- hwpwn/hwpwn.py
+++ hwpwn/hwpwn.py
@@ -1,30 +1,13 @@
-import gzip
-import json
-import logging
-import math
-import os
-import pickle
-from datetime import datetime
-from pprint import pprint
-from typing import Any
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas
 import yaml
-from matplotlib.pyplot import cm
-import csv
-from scipy import signal
-import sys
-import seaborn as sns
-from scipy.spatial.distance import euclidean
 import typer
 
-from . import data
 from . import common
+from . import data
+from . import plot
 
-app = typer.Typer()
+app = typer.Typer(callback=common.default_typer_callback)
 app.add_typer(data.app, name="data")
+app.add_typer(plot.app, name="plot")
 
 
 def export_config(config: dict, filename: str):
